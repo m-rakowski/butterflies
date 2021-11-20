6f50dfdab8ab2ce3816e563376ad32a62ad2fdeb
m-rakowski <michalrakowski@yahoo.com>
2021-11-20 20:56:40 +0000

    closes #8: add a router.post butterfly method and remove ids from 001_seed_butterflies.js

--- server/controllers/butterfly/index.ts
+++ server/controllers/butterfly/index.ts
@@ -13,4 +13,16 @@ router.get('/', async (req, res) => {
     .json(butterflies)
 })
 
+router.post('/', async (req, res) => {
+  try {
+    const [returned] = await knex('butterfly').insert(req.body).returning('id');
+    const newlyCreatedButterfly = await knex('butterfly').where({
+      id: returned,
+    });
+    res.status(201).json(newlyCreatedButterfly);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 export { router };
