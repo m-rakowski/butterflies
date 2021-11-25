752b9cbfdc44ff844b86de27d820ac335fe9c801
m-rakowski <michalrakowski@yahoo.com>
2021-11-25 22:23:15 +0000

    validate POST butterfly and validate CreateButterfly form

--- fe/src/components/Butterfly/Create/index.tsx
+++ fe/src/components/Butterfly/Create/index.tsx
@@ -16,6 +16,7 @@ import {
   VStack,
 } from '@chakra-ui/react';
 import { Butterfly } from '../../../../../server/controllers/butterfly/types';
+import isURL from '../../../../../utils/utils';
 
 const CreateButterfly = () => {
   const navigate = useNavigate();
@@ -55,76 +56,78 @@ const CreateButterfly = () => {
         onSubmit={handleSubmit(sendButterfly)}
       >
         <SimpleGrid columns={2} columnGap={3} rowGap={6} w="full">
-        <GridItem colSpan={colSpan}>
-          <FormControl isInvalid={!!errors.name}>
-            <FormLabel htmlFor="name">Butterfly name</FormLabel>
-            <Input
-              id="name"
-              placeholder="name"
-              {...register('name', {
-                required: 'This is required',
-                minLength: {
-                  value: 4,
-                  message: 'Minimum length should be 4',
-                },
-              })}
-            />
-            <FormErrorMessage>
-              {errors.name && errors.name.message}
-            </FormErrorMessage>
-          </FormControl>
-        </GridItem>
-        <GridItem colSpan={colSpan}>
-          <FormControl isInvalid={!!errors.species}>
-            <FormLabel htmlFor="species">Species</FormLabel>
-            <Input
-              id="species"
-              placeholder="species"
-              {...register('species', {
-                required: 'This is required',
-              })}
-            />
-            <FormErrorMessage>
-              {errors.species && errors.species.message}
-            </FormErrorMessage>
-          </FormControl>
-        </GridItem>
-        <GridItem colSpan={2}>
-          <FormControl isInvalid={!!errors.image_url}>
-            <FormLabel htmlFor="image_url">Link to the picture</FormLabel>
-            <Input
-              id="image_url"
-              placeholder="image_url"
-              {...register('image_url', {
-                required: 'This is required',
-              })}
-            />
-            <FormErrorMessage>
-              {errors.image_url && errors.image_url.message}
-            </FormErrorMessage>
-          </FormControl>
-        </GridItem>
-        <GridItem colSpan={2}>
-          <FormControl isInvalid={!!errors.link}>
-            <FormLabel htmlFor="link">Link to wiki page</FormLabel>
-            <Input
-              id="link"
-              placeholder="link"
-              {...register('link', {
-                required: 'This is required',
-              })}
-            />
-            <FormErrorMessage>
-              {errors.link && errors.link.message}
-            </FormErrorMessage>
-          </FormControl>
-        </GridItem>
-        <GridItem colSpan={2}>
-          <Button mt={4} colorScheme="teal" isLoading={isSubmitting} type="submit">
-            Let's create a butterfly!
-          </Button>
-        </GridItem>
-      </SimpleGrid>
+          <GridItem colSpan={colSpan}>
+            <FormControl isInvalid={!!errors.name}>
+              <FormLabel htmlFor="name">Butterfly name</FormLabel>
+              <Input
+                id="name"
+                placeholder="name"
+                {...register('name', {
+                  required: 'This is required',
+                  minLength: {
+                    value: 4,
+                    message: 'Minimum length should be 4',
+                  },
+                })}
+              />
+              <FormErrorMessage>
+                {errors.name && errors.name.message}
+              </FormErrorMessage>
+            </FormControl>
+          </GridItem>
+          <GridItem colSpan={colSpan}>
+            <FormControl isInvalid={!!errors.species}>
+              <FormLabel htmlFor="species">Species</FormLabel>
+              <Input
+                id="species"
+                placeholder="species"
+                {...register('species', {
+                  required: 'This is required',
+                })}
+              />
+              <FormErrorMessage>
+                {errors.species && errors.species.message}
+              </FormErrorMessage>
+            </FormControl>
+          </GridItem>
+          <GridItem colSpan={2}>
+            <FormControl isInvalid={!!errors.image_url}>
+              <FormLabel htmlFor="image_url">Link to the picture</FormLabel>
+              <Input
+                id="image_url"
+                placeholder="image_url"
+                {...register('image_url', {
+                  validate: (value) => isURL(value) || 'Field value has to be a valid url',
+                  required: 'This is required',
+                })}
+              />
+              <FormErrorMessage>
+                {errors.image_url && errors.image_url.message}
+              </FormErrorMessage>
+            </FormControl>
+          </GridItem>
+          <GridItem colSpan={2}>
+            <FormControl isInvalid={!!errors.link}>
+              <FormLabel htmlFor="link">Link to wiki page</FormLabel>
+              <Input
+                id="link"
+                placeholder="link"
+                {...register('link', {
+                  validate: (value) => isURL(value) || 'Field value has to be a valid url',
+                  required: 'This is required',
+                })}
+              />
+              <FormErrorMessage>
+                {errors.link && errors.link.message}
+              </FormErrorMessage>
+            </FormControl>
+          </GridItem>
+          <GridItem colSpan={2}>
+            <Button mt={4} colorScheme="teal" isLoading={isSubmitting} type="submit">
+              Let's create a butterfly!
+            </Button>
+          </GridItem>
+        </SimpleGrid>
       </form>
     </VStack>
   );
--- server/controllers/butterfly/index.ts
+++ server/controllers/butterfly/index.ts
@@ -1,46 +1,79 @@
-import express from 'express'
-import knex from '../../db'
-import { Butterfly } from './types'
+import express, { Request, Response } from 'express';
+import { body, CustomValidator, validationResult } from 'express-validator';
+import knex from '../../db';
+import { Butterfly } from './types';
+import isURL from '../../../utils/utils';
 
-const router: express.Router = new (express.Router as any)()
+const router: express.Router = new (express.Router as any)();
 
-router.get('/', async (req, res) => {
+async function getAll(req: Request, res: Response) {
   const butterflies = await knex('butterfly')
     .select<Butterfly[]>('*')
     .where(req.query);
+  return res.json(butterflies);
+}
 
-  res
-    .status(200)
-    .json(butterflies)
-})
-
-router.post('/', async (req, res) => {
-  try {
-    const [returned] = await knex('butterfly').insert(req.body).returning('id');
-    const newlyCreatedButterfly = await knex('butterfly').where({
-      id: returned,
-    });
-    res.status(201).json(newlyCreatedButterfly);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
-
-
-router.delete('/:id', async (req, res) => {
+async function deleteButterfly(req: Request, res: Response) {
   if (req.params.id) {
     try {
-      const found = await knex('butterfly').where({ id: req.params.id }).del();
-      res.status(200).send();
+      const found = await knex('butterfly')
+        .where({ id: req.params.id })
+        .del();
+      res.status(200)
+        .send();
     } catch (error) {
-      res.status(404).json(error);
+      res.status(404)
+        .json(error);
     }
   } else {
-    res.status(400).send('Please specify an ID');
+    res.status(400)
+      .send('Please specify an ID');
   }
-});
+}
 
-router.post('/:id/toggle-wishlist-star', async (req, res) => {
+async function findById(id: number) {
+  try {
+    const found = await knex('butterfly')
+      .select<Butterfly>('id')
+      .where({ id });
+    return found;
+  } catch (err) {
+    return null;
+  }
+}
+
+async function findByName(name: string): Promise<Butterfly | null> {
+  try {
+    const found = await knex('butterfly')
+      .select<Butterfly>('*')
+      .where({ name });
+    return found;
+  } catch (err) {
+    return null;
+  }
+}
+
+async function postOne(req: Request, res: Response) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400)
+      .json({ errors: errors.array() });
+  }
+
+  try {
+    const [returned] = await knex('butterfly')
+      .insert(req.body)
+      .returning('id');
+    const newlyCreatedButterfly = await findById(returned.id);
+    res.status(201)
+      .json(newlyCreatedButterfly);
+  } catch (error) {
+    res.status(500)
+      .json(error);
+  }
+}
+
+async function toggleWishlistStar(req: Request, res: Response) {
   if (req.params.id) {
     try {
       const found: Butterfly = await knex('butterfly')
@@ -64,6 +97,48 @@ router.post('/:id/toggle-wishlist-star', async (req, res) => {
     res.status(400)
       .send('Please specify an ID');
   }
-});
+}
+
+export const urlValidator: CustomValidator = (value) => {
+  if (isURL(value)) {
+    return Promise.resolve();
+  }
+  return Promise.reject(new Error('Field value has to be a valid url'));
+};
+export const nameAlreadyTakenValidator: CustomValidator = (value) => {
+  console.log('value', value);
+
+  return findByName(value)
+    .then((butterfly) => {
+      console.log('butterfly isss', butterfly);
+      if (butterfly) {
+        return Promise.reject(new Error('Name has already been used. Please enter a different value'));
+      }
+      return Promise.resolve();
+    });
+};
+
+const postOneValidators = [
+  body('name')
+    .notEmpty()
+    .isLength({ max: 200 })
+    .custom(nameAlreadyTakenValidator),
+  body('species')
+    .notEmpty()
+    .isLength({ max: 200 }),
+  body('image_url')
+    .notEmpty()
+    .isLength({ max: 200 })
+    .custom(urlValidator),
+  body('link')
+    .notEmpty()
+    .isLength({ max: 200 })
+    .custom(urlValidator),
+];
+
+router.get('/', (req, res) => getAll(req, res));
+router.post('/', ...postOneValidators, (req, res) => postOne(req, res));
+router.delete('/:id', async (req, res) => deleteButterfly(req, res));
+router.post('/:id/toggle-wishlist-star', async (req, res) => toggleWishlistStar(req, res));
 
 export { router };
